47266d0699191d63cc92676d6da9061a5433d675
marc zarb <marc.zarb91@gmail.com>
2019-05-18 15:53:45 +0200

    task maker

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Create
+  # Read
+  get 'tasks', to: 'tasks#index', as: :index
+
+  get 'tasks/new', to: 'tasks#new', as: :news
+  post 'tasks', to: 'tasks#create'
+
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # Update
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # Delete
+  delete 'tasks/:id', to: 'tasks#destroy'
+
 end
